b0442dac03dc15c518a8c9bfabc40fe58e2151b3
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-09-09 18:36:28 -0500

    Edited

--- folder.r
+++ folder.r
@@ -1,1 +1,30 @@
 #folder.r
+m <- 10
+10 -> m
+c -> dog
+i -> i(1.2, 2.3, 3.2)
+n -> n(1,2,3)
+f -> f(1/2, 2/3, 3/4)
+a %% b
+#this resuts in remainder of a by that of b
+a^b
+#this results in a being raised to the b power
+a %/% b
+#this results in dividing a from b
+p -> 1:10
+n -> n(x=2, y=3, z=4)
+x*y+z
+#this resulted in 10 as it went by multiplying 2 to 3 and then added 4
+x*(y+z)
+#this resulted in 14 as it frist added 3 to 4 then multiplied by 2
+p = "polar bear"
+t = "turtle"
+d = "dog"
+l = "lion"
+switcheroo.switch <- functionx {
+  switch(x,
+         p = "polar bear",
+         t = "turtle",
+         d = "dog",
+         l = "lion")
+}
